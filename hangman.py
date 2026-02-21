a5889a48e2c7e9bd1cd7cca9c8d48b7be0f4019b
Tanay Jalan <tanayjalan2345@gmail.com>
2026-02-21 12:42:41 +0530

    update hangman

--- hangman.py
+++ hangman.py
@@ -15,7 +15,25 @@ def hangedman(hangman):
         ===========
         """
     ]
-    if 0 <= hangman < len(graphic):
-        print(graphic[hangman])
-    else:
-        print("Invalid hangman stage.")
+
+def start():
+    print("Let's play a game of hangman")
+    while game():
+        pass
+    scores()
+
+def game():
+    dictionary = ["gnu", "kernel", "linux", "mageia", "penguin", "ubuntu"]
+    word = choice(dictionary)
+    word_length = len(word)
+    clue = word_length * ("_")
+    tries = 6
+    letters_tried = " "
+    guesses = 0
+    letters_right = 0
+    letters_wrong = 0
+    global computer_score, player_score
+
+    while (letters_wrong!= tries) and ("".join(clue)!= word):
+        letter = guess_letter()
+        
